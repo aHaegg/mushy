f7ef0d29db34b6f462137f2dba6ae5a96a6ee38e
Andreas Hägg <1733430+aHaegg@users.noreply.github.com>
2024-11-30 23:00:43 +0100

    external function

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   useEffect(() => {
     (async function () {
-      const { text } = await( await fetch(`/api/message`)).json();
+      const { text } = await( await fetch(`/api/HttpTriggerJava?name=Andreas`)).json();
       setData(text);
     })();
   });
