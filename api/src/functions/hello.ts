8c8e7edd3c96f988711417238758441df87586b7
Andreas Hägg <1733430+aHaegg@users.noreply.github.com>
2024-12-08 22:05:41 +0100

    Add database read

--- api/src/functions/hello.ts
+++ api/src/functions/hello.ts
@@ -1,8 +1,33 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { CosmosClient, Item } from '@azure/cosmos';
 
 export async function hello(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
-    return { body: JSON.stringify({text : "Hello, from the API!"}) };
+
+    try {
+        const connectionString = process.env.CosmosDBConnection;
+        if (!connectionString) {
+            throw new Error("CosmosDBConnection environment variable is not defined");
+        }
+        const client = new CosmosClient(connectionString);
+        const database = client.database('mushy-database');
+        const container = database.container('mushy-container');
+
+        const { resource: item } = await container.item("1", "1").read();
+
+        if (!item) {
+            context.log('Document not found');
+            return { status: 404, body: 'Document not found' };
+        }
+
+        context.log('Item:', item);
+
+        return { body: JSON.stringify({ text: "Hello, from the API!", dbtext: item.name }) };
+    } catch (error) {
+        context.log(`Error: ${error}`);
+        return { status: 500, body: 'Internal Server Error' };
+    }
+
 };
 
 app.http('hello', {
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,11 +4,13 @@ import { JSX, useEffect, useState } from 'react';
 export const App = (): JSX.Element => {
 
   const [data, setData] = useState('');
+  const [dbData, setDbData] = useState('');
 
   useEffect(() => {
     (async function () {
-      const { text } = await (await fetch(`/api/hello`)).json();
+      const { text, dbtext } = await (await fetch(`/api/hello`)).json();
       setData(text);
+      setDbData(dbtext);
     })();
   }, []);
 
@@ -17,6 +19,7 @@ export const App = (): JSX.Element => {
     <div>
       <div>Hello {value}</div>
       <div>API: {data}</div>
+      <div>Database: {dbData}</div>
     </div>
   );
 }
