2653c92a8cfee7ec55c428b42efd8d5da69c9ebe
Andreas Hägg <1733430+aHaegg@users.noreply.github.com>
2024-12-08 19:23:26 +0100

    remove old js files in api

--- api/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var functions_1 = require("@azure/functions");
-functions_1.app.setup({
-    enableHttpStream: true,
-});
